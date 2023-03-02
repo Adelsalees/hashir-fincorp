bd800728f934fa549fc094fbe4a6aa5ead3b9b4d
Adelsalees <adelsalees32@gmail.com>
2023-03-02 20:59:21 +0530

    og image

--- resources/views/front/layouts/head.blade.php
+++ resources/views/front/layouts/head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="google-site-verification" content="irvQ1Aqk49UyO7xrbQMuvCfKoocYmHNa3Uvcd8VBWGQ" />
+<meta property="og:image" content="{{  asset('assets/img/favicon.jpg')  }}">
 
 <title>{{ config('app.name', 'Laravel') }} - Registration, Income Tax, Virtual Accounting, Trademark Services</title>
 
